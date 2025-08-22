0f0d940530d8372edb758a83386727fa0dc37a6e
krunchiekrunch <75719839+RadioactivePotato@users.noreply.github.com>
2025-08-22 02:21:35 +0100

    Update main.py

--- firmware/KMK/main.py
+++ firmware/KMK/main.py
@@ -18,9 +18,9 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 keyboard.keymap = [
     [
-        KC.A, KC.B, KC.C, KC.D,
-        KC.E, KC.F, KC.G, KC.H,
-        KC.I, KC.J, KC.K, KC.L,
+        KC.TAB, KC.Q, KC.W, KC.E,
+        KC.LSHIFT, KC.A, KC.S, KC.D,
+        KC.LCTRL, KC.F3, KC.F5, KC.SPACE,
     ]
 ]
 
@@ -38,7 +38,10 @@ keyboard.SDA = board.D4
 display = Display(
     display=SSD1306(sda=board.D4, scl=board.D5),
     entries=[
-        TextEntry(text='krunchpad v1'),
+        TextEntry(text="""
+hello
+world
+        """),
     ],
     height=32,
 )
